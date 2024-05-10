35d3e983ef75209f07d6839f4e13e059ba0e26df
NetBSD User <netbsduser@users.noreply.github.com>
2024-05-10 16:33:54 +0100

    new keyronex sysdeps

--- sysdeps/keyronex/include/keyronex/syscall.h
+++ sysdeps/keyronex/include/keyronex/syscall.h
@@ -3,88 +3,10 @@
 
 #include <stdint.h>
 
-enum posix_syscall {
-	/*! debug print */
-	kPXSysDebug,
-	kPXSysMmap,
-	kPXSysMunmap,
+enum krx_syscall {
+	kKrxDebugMessage,
 
-	kPXSysIOCtl,
-	kPXSysOpenAt,
-	kPXSysClose,
-	kPXSysRead,
-	kPXSysReadLink,
-	kPXSysWrite,
-	kPXSysSeek,
-	kPXSysPPoll,
-	kPXSysIsATTY,
-	kPXSysReadDir,
-	kPXSysStat,
-	kPXSysUnlinkAt,
-	kPXSysGetCWD,
-	kPXSysPipe,
-	kPXSysDup,
-	kPXSysDup3,
-	kPXSysLink,
-	kPXSysChDir,
-	kPXSysUMask,
-	kPXSysMkDirAt,
-	kPXSysRenameAt,
-	kPXSysStatFS,
-	kPXSysFCntl,
-
-	kPXSysSetFSBase,
-	kPXSysExecVE,
-	kPXSysExit,
-	kPXSysFork,
-	kPXSysWaitPID,
-	kPXSysGetPID,
-	kPXSysGetPPID,
-	kPXSysGetPGID,
-	kPXSysSetPGID,
-	kPXSysGetSID,
-	kPXSysSetSID,
-	kPXSysGetTID,
-
-	kPXSysSigAction,
-	kPXSysSigMask,
-	kPXSysSigSend,
-	kPXSysSigSuspend,
-	kPXSysSigTimedWait,
-
-	kPXSysSocket,
-	kPXSysBind,
-	kPXSysConnect,
-	kPXSysListen,
-	kPXSysAccept,
-	kPXSysSendMsg,
-	kPXSysRecvMsg,
-	kPXSysSocketPair,
-	kPXSysGetSockOpt,
-	kPXSysSetSockOpt,
-
-	kPXSysEPollCreate,
-	kPXSysEPollCtl,
-	kPXSysEPollWait,
-
-	kPXSysSleep,
-	kPXSysUTSName,
-	kPXSysClockGet,
-
-	kPXSysForkThread,
-	kPXSysFutexWait,
-	kPXSysFutexWake,
-
-	/*! register signal entry function */
-	kPXSysSigEntry,
-	/*! return from a signal */
-	kPXSysSigReturn,
-};
-
-enum posix_stat_kind {
-	kPXStatKindFD,
-	kPXStatKindAt,
-	kPXStatKindCWD,
+	kKrxVmAllocate,
 };
 
 #if defined(__x86_64__)
@@ -208,6 +130,132 @@ sc_error(uintptr_t ret)
 		return -ret;
 	return 0;
 }
+#else
+
+/*
+ * number: d0
+ * arg 1: d1
+ * arg 2: d2
+ * arg 3: d3
+ * arg 4: d4
+ * arg 5: d5
+ * arg 6: a0
+ * return value: d0
+ * additional output: d1
+ */
+
+static inline uintptr_t
+syscall0(uintptr_t num, uintptr_t *out)
+{
+	(void)num;
+	(void)out;
+	for (;;) ;
+	return 0;
+}
+
+static inline uintptr_t
+syscall1(uintptr_t num, uintptr_t arg1, uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1;
+
+	asm volatile("trap #0\n\t" : "+r"(d0), "+r"(d1)::"memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
+static inline uintptr_t
+syscall2(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1,
+			      d2 asm("d2") = arg2;
+
+	asm volatile("trap #0\n\t" : "+r"(d0), "+r"(d1) : "r"(d2) : "memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
+static inline uintptr_t
+syscall3(intptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3,
+    uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1,
+			      d2 asm("d2") = arg2, d3 asm("d3") = arg3;
+
+	asm volatile("trap #0\n\t"
+		     : "+r"(d0), "+r"(d1)
+		     : "r"(d2), "r"(d3)
+		     : "memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
+static inline uintptr_t
+syscall4(intptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3,
+    uintptr_t arg4, uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1,
+			      d2 asm("d2") = arg2, d3 asm("d3") = arg3,
+			      d4 asm("d4") = arg4;
+
+	asm volatile("trap #0\n\t"
+		     : "+r"(d0), "+r"(d1)
+		     : "r"(d2), "r"(d3), "r"(d4)
+		     : "memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
+static inline uintptr_t
+syscall5(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3,
+    uintptr_t arg4, uintptr_t arg5, uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1,
+			      d2 asm("d2") = arg2, d3 asm("d3") = arg3,
+			      d4 asm("d4") = arg4, d5 asm("d5") = arg5;
+
+	asm volatile("trap #0\n\t"
+		     : "+r"(d0), "+r"(d1)
+		     : "r"(d2), "r"(d3), "r"(d4), "r"(d5)
+		     : "memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
+static inline uintptr_t
+syscall6(uintptr_t num, uintptr_t arg1, uintptr_t arg2, uintptr_t arg3,
+    uintptr_t arg4, uintptr_t arg5, uintptr_t arg6, uintptr_t *out)
+{
+	register uintptr_t d0 asm("d0") = num, d1 asm("d1") = arg1,
+			      d2 asm("d2") = arg2, d3 asm("d3") = arg3,
+			      d4 asm("d4") = arg4, d5 asm("d5") = arg5,
+			      a0 asm("a0") = arg6;
+
+	asm volatile("trap #0\n\t"
+		     : "+r"(d0), "+r"(d1)
+		     : "r"(d2), "r"(d3), "r"(d4), "r"(d5), "r"(a0)
+		     : "memory");
+
+	if (out)
+		*out = d1;
+
+	return d0;
+}
+
 #endif
 
 #endif
